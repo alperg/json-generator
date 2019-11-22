5b71ab174b79829c8c7d6e14d05e45404ab4369a
alperg <mail@alperg.com>
2019-11-22 15:17:41 -0500

    app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+class App extends React.Component {
 
-      </header>
-    </div>
-  );
+  state = {
+    data: this.props.schema
+  };
+
+  render() {
+    const { data } = this.state;
+
+    console.log(data);
+    
+    return (
+      <div className="App">
+        <header className="App-header">
+
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,4 +3,29 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Start schema template
+var DEFAULT_SCHEMA = {
+  "properties": {
+    "id": {
+      "type": "number",
+      "description": "user id"
+    },
+    "email": {
+      "type": "string",
+      "description": "user email"
+    },
+    "age": {
+      "type": "number",
+      "description": "user age"
+    }
+  },
+  "required": [
+    "id",
+    "email"
+  ]
+};
+
+ReactDOM.render(
+  <App schema={DEFAULT_SCHEMA}/>,
+  document.getElementById('root')
+);
